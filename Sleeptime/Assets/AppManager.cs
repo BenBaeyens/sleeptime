f8d661cfa3ecc0a64d51ababcf310128b36ffba4
Kjip <kjipgamer@gmail.com>
2019-05-19 18:24:31 +0200

    Added sleepmenu animation

---
+++ Sleeptime/Assets/AppManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AppManager : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SleepNow() {
+        animator.SetTrigger("Sleepnow");
+    }
+
+}
