06983de339273516b94811e5a8fdc05f9e20464b
Kjip <kjipgamer@gmail.com>
2019-05-19 18:55:54 +0200

    Added sleep now functionality

--- Sleeptime/Assets/AppManager.cs
+++ Sleeptime/Assets/AppManager.cs
@@ -2,17 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
+using System;
 
-public class AppManager : MonoBehaviour
-{
+public class AppManager : MonoBehaviour {
     Animator animator;
+
+  
+   
+    string time;
+    int minutes;
+    int hours;
 
     private void Start() {
         animator = GetComponent<Animator>();
+        
+        
+
+        CalculateTime(1);
+        CalculateTime(2);
+        CalculateTime(3);
+            }
+
+    private void Update() {
+      
+
+
+
+  
+       
+
+
+
+       
     }
 
     public void SleepNow() {
         animator.SetTrigger("Sleepnow");
+    }
+
+    public void CalculateTime(int t) {
+        string[] temp = DateTime.Now.TimeOfDay.ToString().Split(':');
+        hours = int.Parse(temp[0]);
+        minutes = int.Parse(temp[1]);
+
+        for (int i = 0; i < t; i++)
+        {
+
+        hours += 1;
+        minutes += 30;
+          
+
+        if(minutes >= 60)
+        {
+            minutes -= 60;
+            hours += 1;
+        }
+        if (hours >= 24)
+            hours -= 24;
+        }
+
+        time = string.Format("{0:00}:{1:00}", hours, minutes);
+        Debug.Log(time);
     }
 
 }
