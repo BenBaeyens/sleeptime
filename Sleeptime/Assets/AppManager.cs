c6e11401e031a9481d2c322e86ac1136c74bb666
Kjip <kjipgamer@gmail.com>
2019-05-19 20:38:55 +0200

    Finished wake up time mechanics

--- Sleeptime/Assets/AppManager.cs
+++ Sleeptime/Assets/AppManager.cs
@@ -3,41 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 using System;
+using TMPro;
 
 public class AppManager : MonoBehaviour {
     Animator animator;
 
-  
-   
+
+    public TMP_InputField wakeupTime;
+    public string[] temparray;
     public string time;
     int minutes;
     int hours;
 
     private void Start() {
         animator = GetComponent<Animator>();
-        
-        
+
+
 
         CalculateTime(1);
         CalculateTime(2);
         CalculateTime(3);
-            }
-
-    private void Update() {
-      
-
-
-
-  
-       
-
-
-
-       
     }
 
- 
-    
+    private void Update() {
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public void QuitApp() {
+        Application.Quit();
+    }
 
     public void CalculateTime(int t) {
         string[] temp = DateTime.Now.TimeOfDay.ToString().Split(':');
@@ -48,21 +52,54 @@ public class AppManager : MonoBehaviour {
         for (int i = 0; i < t; i++)
         {
 
-        hours += 1;
-        minutes += 40;
-          
-
-        if(minutes >= 60)
-        {
-            minutes -= 60;
             hours += 1;
-        }
-        if (hours >= 24)
-            hours -= 24;
+            minutes += 40;
+
+
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours += 1;
+            }
+            if (hours >= 24)
+                hours -= 24;
         }
 
         time = string.Format("{0:00}:{1:00}", hours, minutes);
         Debug.Log(time);
     }
 
+    public void CalculateWakeUpTime(int t) {
+        minutes -= 14;
+        for (int i = 0; i < t; i++)
+        {
+
+            hours -= 1;
+            minutes -= 40;
+
+
+            if (minutes < 0)
+            {
+                minutes += 60;
+                hours -= 1;
+            }
+            if (hours < 0)
+                hours += 24;
+        }
+
+        time = string.Format("{0:00}:{1:00}", hours, minutes);
+
+    }
+
+    public void wakeUpFixText() {
+
+
+        string firstpart = wakeupTime.text.Substring(0, 2);
+        string secondpart = wakeupTime.text.Substring(2, 2);
+        hours = int.Parse(firstpart);
+        minutes = int.Parse(secondpart);
+        wakeupTime.text = firstpart + ":" + secondpart;
+
+   
+    }
 }
